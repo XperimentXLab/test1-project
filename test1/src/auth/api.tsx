3c88af07fea67adaf4ef27421a9d967d19535762
XperimentXLab <xperimentxlab@gmail.com>
2025-05-11 00:28:01 +0800

    url update

--- test1/src/auth/api.tsx
+++ test1/src/auth/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = 'http://localhost:8080/server'
+export const baseURL = 'https://test1-project.vercel.app//server'
 const tokenURL = `${baseURL}/token/refresh/`
 
 axios.defaults.xsrfCookieName = 'csrftoken';
